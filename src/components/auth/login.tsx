f159022de62c06aa1934c81cb3781dd39ff22ab8
piesieomane <piesieyeboah@gmail.com>
2023-01-25 19:48:25 +0000

    setup the login page

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 // import { useDispatch } from 'react-redux';
 // import { login, signup } from '../store/actions/auth';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
@@ -9,7 +10,7 @@ const LoginPage = () => {
   const [confirmPassword, setConfirmPassword] = useState('');
   const [isSignup, setIsSignup] = useState(false);
   const router = useRouter();
-  // const dispatch = useDispatch();
+  //   const dispatch = useDispatch();
 
   //   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
   //     e.preventDefault();
@@ -27,126 +28,131 @@ const LoginPage = () => {
   //   };
 
   return (
-    <div className="bg-gray-900 min-h-screen flex flex-col justify-center">
-      {isSignup ? (
-        <form
-          className="bg-white p-6 rounded-lg shadow-md"
-          // onSubmit={handleSubmit}
-        >
-          <h1 className="text-lg font-medium mb-4">Signup</h1>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-medium mb-2"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="border border-gray-400 p-2 w-full"
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-medium mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="border border-gray-400 p-2 w-full"
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-medium mb-2"
-              htmlFor="confirmPassword"
-            >
-              Confirm Password
-            </label>
-            <input
-              className="border border-gray-400 p-2 w-full"
-              type="password"
-              id="confirmPassword"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-          </div>
-          <button
-            className="bg-indigo-500
-text-white py-2 px-4 rounded-lg hover:bg-indigo-600"
+    <div className="bg-gray-900 min-h-screen flex flex-col justify-center items-center">
+      <AnimatePresence>
+        {isSignup ? (
+          <motion.form
+            className="bg-white p-6 rounded-lg w-2/5 shadow-md"
+            // onSubmit={handleSubmit}
+            initial={{ scale: 0.5, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0.5, opacity: 0 }}
           >
-            Signup
-          </button>
-          <p className="text-center text-gray-500 mt-4">
-            Already have an account?
-            <a
-              href="#"
-              className="text-indigo-600 underline"
-              onClick={() => setIsSignup(false)}
-            >
-              Login
-            </a>
-          </p>
-        </form>
-      ) : (
-        <form
-          className="bg-white p-6 rounded-md shadow-md"
-          //  onSubmit={handleSubmit}
-        >
-          <h1 className="text-lg font-medium mb-4">Login</h1>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-medium mb-2"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="border border-gray-400 p-2 w-full"
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-medium mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="border border-gray-400 p-2 w-full"
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button className="bg-indigo-500 text-white py-2 px-4 rounded-lg hover:bg-indigo-600">
-            Login
-          </button>
-          <p className="text-center text-gray-500 mt-4">
-            Don't have an account?
-            <a
-              href="#"
-              className="text-indigo-600 underline"
-              onClick={() => setIsSignup(true)}
-            >
+            <h1 className="text-lg font-medium mb-4">Signup</h1>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 font-medium mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="border border-gray-400 p-2 w-full"
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 font-medium mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="border border-gray-400 p-2 w-full"
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 font-medium mb-2"
+                htmlFor="confirmPassword"
+              >
+                Confirm Password
+              </label>
+              <input
+                className="border border-gray-400 p-2 w-full"
+                type="password"
+                id="confirmPassword"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+            <button className="bg-indigo-500 text-white py-2 px-4 rounded-lg hover:bg-indigo-600">
               Signup
-            </a>
-          </p>
-        </form>
-      )}
+            </button>
+            <p className="text-center text-gray-500 mt-4">
+              Already have an account?
+              <a
+                href="#"
+                className="text-indigo-600 underline"
+                onClick={() => setIsSignup(false)}
+              >
+                Login
+              </a>
+            </p>
+          </motion.form>
+        ) : (
+          <motion.form
+            className="bg-white p-6 rounded-lg w-2/5 shadow-md"
+            // onSubmit={handleSubmit}
+            initial={{ scale: 0.5, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0.5, opacity: 0 }}
+          >
+            <h1 className="text-lg font-medium mb-4">Login</h1>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 font-medium mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="border border-gray-400 p-2 w-full"
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 font-medium mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="border border-gray-400 p-2 w-full"
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button className="bg-indigo-500 text-white py-2 px-4 rounded-lg hover:bg-indigo-600">
+              Login
+            </button>
+            <p className="text-center text-gray-500 mt-4">
+              Don't have an account?
+              <a
+                href="#"
+                className="text-indigo-600 underline"
+                onClick={() => setIsSignup(true)}
+              >
+                Signup
+              </a>
+            </p>
+          </motion.form>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
