6ea0e47f7d30db300e185e6864d2cf16434c05bc
piesieomane <piesieyeboah@gmail.com>
2023-01-27 19:35:03 +0000

    Install material ui

---
+++ src/data/index.js
@@ -0,0 +1,42 @@
+import { faker } from '@faker-js/faker';
+import {
+  NoteBlank,
+  UserSquare,
+  ChartBar,
+  ChatCircleText,
+  Gear,
+  Phone,
+  BellRinging,
+  SignOut,
+  User,
+  Users,
+} from 'phosphor-react';
+
+const Nav_Buttons = [
+  {
+    index: 0,
+    icon: <NoteBlank />,
+  },
+  {
+    index: 1,
+    icon: <UserSquare />,
+  },
+  {
+    index: 2,
+    icon: <ChartBar />,
+  },
+  {
+    index: 3,
+    icon: <ChatCircleText />,
+  },
+  {
+    index: 4,
+    icon: <Gear />,
+  },
+  {
+    index: 5,
+    icon: <Phone />,
+  },
+];
+
+export { Nav_Buttons };
